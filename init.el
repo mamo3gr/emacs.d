839550e0283a77158549d3037856288b946403cf
Mamoru Miura <mamo0110@gmail.com>
2019-03-04 22:57:55 +0900

    introduce use-package and doom-themes.

--- init.el
+++ init.el
@@ -26,7 +26,7 @@ There are two things you can do about this warning:
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (init-loader))))
+ '(package-selected-packages (quote (doom-themes use-package init-loader))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
