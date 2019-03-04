ed4a1e43729abf44d11d8e686fb72a62ea702483
Mamoru Miura <mamo0110@gmail.com>
2019-03-04 21:54:40 +0900

    introduce init-loader.

--- init.el
+++ init.el
@@ -15,3 +15,20 @@ There are two things you can do about this warning:
     ;; For important compatibility libraries like cl-lib
     (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
 (package-initialize)
+
+;;;; init-loader
+(require 'init-loader)
+(init-loader-load "~/.emacs.d/inits")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (init-loader))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
