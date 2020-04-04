50480ecd94272ea3544c6e385c7acf63679daa9e
Mamoru Miura <mamo0110+github@gmail.com>
2020-04-05 08:07:08 +0900

    :tada: add settings for display

---
+++ inits/20_display.el
@@ -0,0 +1,78 @@
+;;;; display
+
+;; theme
+(use-package ample-theme
+  :init (progn (load-theme 'ample t t)
+               (load-theme 'ample-flat t t)
+               (load-theme 'ample-light t t)
+               (enable-theme 'ample-flat))
+  :defer t
+  :ensure t)
+
+;; frame
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(setq default-frame-alist
+      (append (list
+               '((font . "Cica-16")
+                 (width . 95)
+                 (height . 50)))
+              default-frame-alist))
+
+;; buffer
+(setq inhibit-startup-message t)
+
+;; modeline
+(setq eol-mnemonic-dos "(CRLF)")
+(setq eol-mnemonic-mac "(CR)")
+(setq eol-mnemonic-unix "(LF)")
+
+(setq line-number-mode t)
+(setq column-number-mode t)
+
+;; minibuffer
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; frange
+(setq-default indicate-empty-lines t)
+(setq-default indicate-buffer-boundaries 'left)
+
+;; cursor
+(blink-cursor-mode -1)
+
+;;;; paren
+(use-package paren
+  :config
+  (show-paren-mode 1)
+  :custom
+  (show-paren-style 'parenthesis))
+
+;;;; uniquify
+;(setq uniquify-buffer-name-style 'post-forward-angle-brackets)  ;; filename<dir>
+(setq uniquify-buffer-name-style 'forward)                       ;; dir/filename
+(setq uniquify-strip-common-suffix t)
+(setq uniquify-min-dir-content 2)
+
+;;;; line-numbers
+(if (version<= "26.0.50" emacs-version)
+    (progn
+      (global-display-line-numbers-mode)
+      (set-face-attribute 'line-number nil
+                          :inherit font-lock-comment-face)
+      (set-face-attribute 'line-number-current-line nil
+                          :inherit font-lock-keyword-face)))
+
+;;;; rainbow-delimiters
+(use-package rainbow-delimiters
+  :hook
+  (prog-mode . rainbow-delimiters-mode))
+
+(use-package whitespace
+  :config
+  (global-whitespace-mode 1)
+  :custom
+  (whitespace-style '(face
+                      trailing
+                      tabs
+                      empty
+                      tab-mark)))
