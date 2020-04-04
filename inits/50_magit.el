d900e2ab9af3fb2e97d24fcda0d14e49e55a6054
Mamoru Miura <mamo0110+github@gmail.com>
2020-04-05 08:03:19 +0900

    :tada: introduce magit and git-gutter

---
+++ inits/50_magit.el
@@ -0,0 +1,18 @@
+;;;; magit
+(use-package magit
+  :ensure t
+  :bind
+  ("C-c m" . magit))
+
+(use-package git-gutter
+  :ensure t
+  :custom
+  (git-gutter:modified-sign "~")
+  (git-gutter:added-sign    "+")
+  (git-gutter:deleted-sign  "-")
+  :custom-face
+  (git-gutter:modified ((t (:foreground "#f1fa8c" :background "#818a0c"))))
+  (git-gutter:added    ((t (:foreground "#50fa7b" :background "#30da5b"))))
+  (git-gutter:deleted  ((t (:foreground "#ff79c6" :background "#df59a6"))))
+  :config
+  (global-git-gutter-mode 1))
