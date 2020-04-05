241b635b29f3eddea7482e2bc60015b1cf375bb6
Mamoru Miura <mamo0110+github@gmail.com>
2020-04-05 09:08:50 +0900

    :tada: introduce ivy and counsel

---
+++ inits/30_ivy.el
@@ -0,0 +1,20 @@
+;;;; ivy/counsel
+(use-package ivy
+  :ensure t
+  :config
+  (ivy-mode 1)
+  :bind
+  ("M-o" . swiper)
+  ("C-c C-r" . ivy-resume)
+  :custom
+  (ivy-count-format "(%d/%d) "))
+
+(use-package counsel
+  :ensure t
+  :config
+  ;; remaps built-in emacs functions that have counsel replacements
+  (counsel-mode 1)
+  :bind
+  ([remap recentf-open-files] . counsel-recentf)
+  ([remap yank-pop] . counsel-yank-pop)
+  ([remap find-file] . counsel-find-file))
