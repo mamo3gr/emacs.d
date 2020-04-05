8cc2a4d5743bb41d6f63b42a485e9e5ab69a26d6
Mamoru Miura <mamo0110+github@gmail.com>
2020-04-05 14:01:04 +0900

    :recycle: deactivate defalut key bind

--- inits/16_undo-tree.el
+++ inits/16_undo-tree.el
@@ -3,7 +3,7 @@
   :config
   (global-undo-tree-mode 1)
   :bind
-  ([remap undo] . undo-tree-undo)
+  ;; ([remap undo] . undo-tree-undo)  :: binded by default
   ("M-/" . undo-tree-redo)
   ;; ("C-x u" . undo-tree-visualize)  ;; binded by default
   )
