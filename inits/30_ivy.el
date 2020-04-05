883fffffec9aa899abd58418b6cd98bf553a58a7
Mamoru Miura <mamo0110+github@gmail.com>
2020-04-05 14:02:05 +0900

    :+1: show icons for ivy

--- inits/30_ivy.el
+++ inits/30_ivy.el
@@ -16,3 +16,15 @@
   ([remap recentf-open-files] . counsel-recentf)
   ([remap yank-pop] . counsel-yank-pop)
   ([remap find-file] . counsel-find-file))
+
+(use-package all-the-icons-ivy-rich
+  :after ivy
+  :config
+  (all-the-icons-ivy-rich-mode 1)
+  :custom
+  (all-the-icons-ivy-rich-icon-size 0.7))
+
+(use-package ivy-rich
+  :after ivy
+  :config
+  (ivy-rich-mode 1))
