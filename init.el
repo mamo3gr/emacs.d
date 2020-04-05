5cc8d50e27422daea62834b78dbb48c58d9c9503
Mamoru Miura <mamo0110+github@gmail.com>
2020-04-05 09:23:52 +0900

    :tada: add inits-dir for init-loader

--- init.el
+++ init.el
@@ -18,17 +18,16 @@ There are two things you can do about this warning:
 
 ;;;; init-loader
 (require 'init-loader)
-(init-loader-load "~/.emacs.d/inits")
+(setq inits-dir (expand-file-name "~/.emacs.d/inits/"))
+(setq init-loader-show-log-after-init t)
+(init-loader-load inits-dir)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (init-loader))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+ '(package-selected-packages
+   (quote
+    (ample-theme git-gutter magit recentf-mode company dockerfile-mode markdown-mode counsel rainbow-delimiters use-package auto-highlight-symbol anzu mozc init-loader))))
+
