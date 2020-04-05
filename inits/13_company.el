f0779d80cb9e1bea7f5594f27b574e0c1f5ad5c4
Mamoru Miura <mamo0110+github@gmail.com>
2020-04-05 11:27:51 +0900

    :+1: do not downcase the returned candidates

--- inits/13_company.el
+++ inits/13_company.el
@@ -6,18 +6,16 @@
   (company-dabbrev-downcase nil)  ;; do not downcase the returned candidates
   (company-idle-delay 0)
   (company-minimum-prefix-length 2)
-  (company-selection-wrap-around t)
   :bind
-  ("C-M-i" . company-complete)
   (:map company-active-map
         ("C-n" . company-select-next)
         ("C-p" . company-select-previous)
         ("C-s" . company-filter-candidates)
         ("C-i" . company-complete-selection)
-        ([tab] . company-complete-selection))
+        ([?\t] . company-complete-selection))
   (:map company-search-map
         ("C-n" . company-select-next)
         ("C-p" . company-select-previous)
         ("C-i" . company-complete-selection)
-        ([tab] . company-complete-selection))
+        ([?\t] . company-complete-selection))
   )
