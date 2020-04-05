23c51fee55e477eea34ba4d919034cb6ff62575f
Mamoru Miura <mamo0110+github@gmail.com>
2020-04-05 14:56:08 +0900

    :recycle: refactor anzu setting

--- inits/14_anzu.el
+++ inits/14_anzu.el
@@ -1,8 +1,9 @@
 ;;;; anzu
 (use-package anzu
-  :config
-  (global-anzu-mode 1)
   :diminish
+  :hook (after-init . global-anzu-mode)
+  :custom-face
+  (anzu-mode-line ((t (:foreground "DeepPink4"))))
   :bind
   ([remap query-replace] . anzu-query-replace)
   ([remap query-replace-regexp] . anzu-query-replace-regexp))
