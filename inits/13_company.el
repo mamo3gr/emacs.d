07fb6731ecf5565870ee818003ab6ff4c96d7323
Mamoru Miura <mamo0110+github@gmail.com>
2020-04-05 11:28:32 +0900

    :+1: add key bind

--- inits/13_company.el
+++ inits/13_company.el
@@ -8,15 +8,16 @@
   (company-minimum-prefix-length 2)
   (company-selection-wrap-around t)
   :bind
+  ("C-M-i" . company-complete)
   (:map company-active-map
         ("C-n" . company-select-next)
         ("C-p" . company-select-previous)
         ("C-s" . company-filter-candidates)
         ("C-i" . company-complete-selection)
-        ([?\t] . company-complete-selection))
+        ([tab] . company-complete-selection))
   (:map company-search-map
         ("C-n" . company-select-next)
         ("C-p" . company-select-previous)
         ("C-i" . company-complete-selection)
-        ([?\t] . company-complete-selection))
+        ([tab] . company-complete-selection))
   )
