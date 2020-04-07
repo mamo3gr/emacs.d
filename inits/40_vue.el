26b7efef0185f62039606c8e1d9cf1346e3825d4
Mamoru Miura <mamo0110+github@gmail.com>
2020-04-07 20:36:27 +0900

    :tada: add vue mode

---
+++ inits/40_vue.el
@@ -0,0 +1,8 @@
+;;;; vue-mode
+(use-package vue-mode
+  :hook
+  (vue-mode . company-mode)
+  :init
+  ;; avoid identation problem
+  ;;   Ref: https://qiita.com/akicho8/items/58c2ac5d762a2a4479c6
+  (add-hook 'vue-mode-hook (lambda () (setq syntax-ppss-table nil))))
