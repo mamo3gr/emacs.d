b2ffb403b83830ba1fdfebd28d84475c64497918
Mamoru Miura <mamo0110+github@gmail.com>
2020-04-09 20:50:55 +0900

    :+1: enable smartparens-mode in vue-mode

--- inits/40_vue.el
+++ inits/40_vue.el
@@ -1,7 +1,8 @@
 ;;;; vue-mode
 (use-package vue-mode
   :hook
-  (vue-mode . company-mode)
+  ((vue-mode . company-mode)
+   (vue-mode . smartparens-mode))
   :init
   ;; avoid identation problem
   ;;   Ref: https://qiita.com/akicho8/items/58c2ac5d762a2a4479c6
