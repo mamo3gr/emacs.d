cc1fc18ec6c60e5301882e4b37b61ea4e320b3d5
Mamoru Miura <mamo3gr@gmail.com>
2021-03-13 17:06:34 +0900

    :tada: Introduce org-mode

---
+++ inits/40_org.el
@@ -0,0 +1,65 @@
+;;;; org
+(use-package org
+  :commands org-agenda
+  :mode (("\\.org\\'" . org-mode))
+  :init
+  (setq org-directory "~/org/")
+  (setq org-default-notes-file (concat org-directory "main.org"))
+  (setq org-agenda-files (list org-directory))
+  (defun my:org-goto-inbox ()
+    (interactive)
+    (find-file org-default-notes-file))
+  :config
+  (setq system-time-locale "C")  ;; to avoid Japanese in the time stamp
+  (setq org-hide-leading-stars t)
+  (setq org-startup-folded nil)
+  ;; org-capture and enrty
+  (setq org-todo-keywords
+        '((sequence "TODO(t)" "WAIT(w)" "|" "DONE(d)" "ABORT(a)" "SOMEDAY(s)")))
+  (setq org-tag-alist '(("PROJECT" . ?p) ("MEMO" . ?m) ("PETIT" . ?t)))
+  (setq org-capture-templates
+        '(("t" "Task" entry (file+headline org-default-notes-file "inbox")
+           "** TASK %?\n   CREATED: %U\n")
+          ("i" "Idea" entry (file+headline org-default-notes-file "idea")
+           "** %?\n   CREATED: %U\n")))
+  (setq org-refile-targets '((org-agenda-files :maxlevel . 1)))
+  ;; org-clock
+  (setq org-log-done 'time)  ;; add a time stamp to the task when done
+  (setq org-clock-clocked-in-display 'frame-title)
+  :bind
+  (("C-c a" . org-agenda)
+   ("C-c c" . org-capture)
+   ("C-c g" . org-clock-goto)
+   ("C-c i" . my:org-goto-inbox)
+   :map org-mode-map
+   ("C-m" . org-return-indent)
+   ("M-n" . org-forward-same-level)
+   ("M-p" . org-backward-same-level)
+   ("C-c h" . helm-org-in-buffer-headings)))
+
+(use-package org-agenda
+  :ensure nil
+  :commands org-agenda
+  :config
+  (setq org-agenda-custom-commands
+        '(("x" "Unscheduled Tasks" tags-todo
+           "-SCHEDULED>=\"<today>\"-DEADLINE>=\"<today>\"" nil)
+          ("d" "Daily Tasks" agenda ""
+           ((org-agenda-span 1)))))
+  (setq org-agenda-skip-scheduled-if-done t)
+  (setq org-return-follows-link t)  ;; RET to follow link
+  (setq org-agenda-columns-add-appointments-to-effort-sum t)
+  (setq org-agenda-time-grid
+        '((daily today require-timed)
+          (0800 1200 1300 1700) "......" "----------------"))
+  (setq org-columns-default-format
+        "%68ITEM(Task) %6Effort(Effort){:} %6CLOCKSUM(Clock){:}")
+  (defadvice org-agenda-switch-to (after org-agenda-close)
+    "Close a org-agenda window when RET is hit on the window."
+    (progn (delete-other-windows)
+           (recenter-top-bottom)))
+  (ad-activate 'org-agenda-switch-to)
+  :bind
+  (:map org-agenda-mode-map
+        ("s" . org-agenda-schedule)
+        ("S" . org-save-all-org-buffers)))
