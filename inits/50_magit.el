6bbb16690d70193faf03725d7720841fecdf1d2a
Mamoru Miura <mamo3gr@gmail.com>
2022-07-15 16:23:54 +0900

    :recycle: Cut out git-gutter setting

--- inits/50_magit.el
+++ inits/50_magit.el
@@ -2,16 +2,3 @@
 (use-package magit
   :bind
   ("C-c m" . magit))
-
-(use-package git-gutter
-  :diminish
-  :custom
-  (git-gutter:modified-sign "~")
-  (git-gutter:added-sign    "+")
-  (git-gutter:deleted-sign  "-")
-  :custom-face
-  (git-gutter:modified ((t (:foreground "#f1fa8c" :background "#818a0c"))))
-  (git-gutter:added    ((t (:foreground "#50fa7b" :background "#30da5b"))))
-  (git-gutter:deleted  ((t (:foreground "#ff79c6" :background "#df59a6"))))
-  :config
-  (global-git-gutter-mode 1))
