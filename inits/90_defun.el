4bb3321b73a013cc27331f6217d6242aea3d69fd
Mamoru Miura <mamo3gr@gmail.com>
2022-07-17 11:40:21 +0900

    :sparkles: Add a function for describing function/variable

--- inits/90_defun.el
+++ inits/90_defun.el
@@ -57,3 +57,36 @@ Repeated invocations toggle between the two most recently open buffers."
   (interactive)
   (switch-to-buffer (other-buffer (current-buffer) 1)))
 (define-key global-map (kbd "C-x C-b") 'er-switch-to-previous-buffer)
+
+;; Describe function/variable or function call
+;; Reference:
+;;   [Home] Describe Thing At Point
+;;   https://www.emacswiki.org/emacs/DescribeThingAtPoint
+(defun describe-foo-at-point ()
+  "Show the documentation of the Elisp function and variable near point.
+This checks in turn:
+-- for a function name where point is
+-- for a variable name where point is
+-- for a surrounding function call
+"
+  (interactive)
+  (let (sym)
+    ;; sigh, function-at-point is too clever.    we want only the first half.
+    (cond ((setq sym (ignore-errors
+                       (with-syntax-table emacs-lisp-mode-syntax-table
+                         (save-excursion
+                           (or (not (zerop (skip-syntax-backward "_w")))
+                               (eq (char-syntax (char-after (point))) ?w)
+                               (eq (char-syntax (char-after (point))) ?_)
+                               (forward-sexp -1))
+                           (skip-chars-forward "`'")
+                           (let ((obj (read (current-buffer))))
+                             (and (symbolp obj) (fboundp obj) obj))))))
+           (describe-function sym))
+          ((setq sym (variable-at-point)) (describe-variable sym))
+          ;; now let it operate fully -- i.e. also check the
+          ;; surrounding sexp for a function call.
+          ((setq sym (function-at-point)) (describe-function sym)))))
+(define-key emacs-lisp-mode-map [(f1)] 'describe-foo-at-point)
+(define-key emacs-lisp-mode-map [(control f1)] 'describe-function)
+(define-key emacs-lisp-mode-map [(shift f1)] 'describe-variable)
