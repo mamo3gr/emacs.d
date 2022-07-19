9ab3d3ae203e3b17656276cc3b4dce7d675d5462
Mamoru Miura <mamo3gr@gmail.com>
2022-07-19 21:23:03 +0900

    :wrench: Avoid saving undo-tree history

--- inits/16_undo-tree.el
+++ inits/16_undo-tree.el
@@ -2,6 +2,8 @@
 (use-package undo-tree
   :config
   (global-undo-tree-mode 1)
+  :custom
+  (undo-tree-auto-save-history nil "do not save history")
   :bind
   ;; ([remap undo] . undo-tree-undo)  :: binded by default
   ("M-/" . undo-tree-redo)
