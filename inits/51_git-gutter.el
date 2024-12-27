f7e2ce611692ae21ffd8ee29cb38eb07431c7496
Mamoru Miura <mamoru-miura@m3.com>
2024-12-28 08:18:28 +0900

    chore: set key bindings for git-gutter

--- inits/51_git-gutter.el
+++ inits/51_git-gutter.el
@@ -10,4 +10,9 @@
   (git-gutter:added    ((t (:foreground "#50fa7b" :background "#30da5b"))))
   (git-gutter:deleted  ((t (:foreground "#ff79c6" :background "#df59a6"))))
   :config
-  (global-git-gutter-mode 1))
+  (global-git-gutter-mode 1)
+  :bind
+  (("C-c d" . git-gutter:popup-hunk)
+   ("C-c p" . git-gutter:previous-hunk)
+   ("C-c n" . git-gutter:next-hunk)
+   ("C-c r" . git-gutter:revert-hunk)))
